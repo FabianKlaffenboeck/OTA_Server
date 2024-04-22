263c878462590e5cf9e736e0016d7e95b811ae28
Fabiankla <fabian@klaffenboeck.eu>
2024-04-22 19:37:52 +0200

    changed device id to uuid

--- src/main/kotlin/at/fklab/model/EndDevice.kt
+++ src/main/kotlin/at/fklab/model/EndDevice.kt
@@ -2,33 +2,35 @@ package at.fklab.model
 
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.UUIDEntity
+import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.javatime.datetime
+import java.util.*
 
 
-object EndDevices : IntIdTable("Trays") {
-    val name = varchar("name", 100)
+object EndDevices : UUIDTable("EndDevices") {
+    val info = varchar("info", 100)
 
     val updatedAt = datetime("updatedAt").nullable()
     val deletedAt = datetime("deletedAt").nullable()
 }
 
-class EndDeviceEntity(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<EndDeviceEntity>(EndDevices)
+class EndDeviceEntity(id: EntityID<UUID>) : UUIDEntity(id) {
+    companion object : UUIDEntityClass<EndDeviceEntity>(EndDevices)
 
-    var name by EndDevices.name
+    var info by EndDevices.info
 
     var updatedAt by EndDevices.updatedAt
     var deletedAt by EndDevices.deletedAt
 
     fun toEndDevice() = EndDevice(
-        id.value,
-        name
+        id.value, info
     )
 }
 
 class EndDevice(
-    var id: Int?,
-    var name: String
+    var id: UUID?,
+    var info: String
 )
