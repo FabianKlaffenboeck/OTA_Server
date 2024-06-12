f789efb9f179b1002f20ec1361aa20949b98c158
Fabian <fabian@klaffenboeck.eu>
2024-06-12 19:29:01 +0200

    added testdata

---
+++ src/main/kotlin/at/fklab/ota_server/development/accessToken_samples.kt
@@ -0,0 +1,11 @@
+package at.fklab.ota_server.development
+
+import at.fklab.ota_server.models.AccessToken
+
+val sampleAccessTokens = listOf<AccessToken>(
+    AccessToken(id = 1, tokenString = "", info = ""),
+    AccessToken(id = 2, tokenString = "", info = ""),
+    AccessToken(id = 3, tokenString = "", info = ""),
+    AccessToken(id = 4, tokenString = "", info = ""),
+    AccessToken(id = 5, tokenString = "", info = ""),
+)
---
+++ src/main/kotlin/at/fklab/ota_server/development/user_samples.kt
@@ -0,0 +1,8 @@
+package at.fklab.ota_server.development
+
+import at.fklab.ota_server.models.User
+
+val sampleUsers = listOf<User>(
+    User(id = 1, accessToken = sampleAccessTokens[0], info = ""),
+    User(id = 2, accessToken = sampleAccessTokens[1], info = ""),
+)
