91f0bb425d4cbec2d1728256fddd7a2d913bb5ad
Fabian <fabian@klaffenboeck.eu>
2024-06-16 14:41:59 +0200

    hopefully fixed tests

--- src/main/kotlin/at/fklab/ota_server/services/FileService.kt
+++ src/main/kotlin/at/fklab/ota_server/services/FileService.kt
@@ -3,6 +3,9 @@ package at.fklab.ota_server.services
 import at.fklab.ota_server.models.FirmwareRelease
 import java.io.File
 import java.io.InputStream
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.exists
 
 class FileService(val pathToFile: String) {
 
@@ -18,7 +21,11 @@ class FileService(val pathToFile: String) {
     }
 
     fun save(fileData: InputStream, firmwareRelease: FirmwareRelease) {
-        val file = File(pathToFile, generateFileName(firmwareRelease))
+
+        val path = Path(pathToFile)
+        if (!path.exists()) path.createDirectories()
+
+        val file = File(pathToFile + generateFileName(firmwareRelease))
         file.writeBytes(fileData.readBytes())
     }
 }
